c481b57178551f448db9c8471e7e05e5c670da69
Ferdinand Malcher <ferdinand@malcher.media>
2023-11-09 12:25:03 +0100

    Implementierung BookRatingService

--- book-rating/src/app/books/shared/book-rating.service.ts
+++ book-rating/src/app/books/shared/book-rating.service.ts
@@ -9,10 +9,16 @@ export class BookRatingService {
   constructor() { }
 
   rateUp(book: Book): Book {
-    return book; // TODO
+    return {
+      ...book,
+      rating: book.rating < 5 ? book.rating + 1 : 5
+    };
   }
 
   rateDown(book: Book): Book {
-    return book; // TODO
+    return {
+      ...book,
+      rating: Math.max(book.rating - 1, 1)
+    };
   }
 }
