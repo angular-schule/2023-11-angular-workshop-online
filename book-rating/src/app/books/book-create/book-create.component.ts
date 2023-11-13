e898035d91ab910c791e3527da86bf0432d93d7c
Ferdinand Malcher <ferdinand@malcher.media>
2023-11-13 12:34:12 +0100

    HTML-Formular

--- book-rating/src/app/books/book-create/book-create.component.ts
+++ book-rating/src/app/books/book-create/book-create.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-book-create',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './book-create.component.html',
   styleUrls: ['./book-create.component.scss']
 })
