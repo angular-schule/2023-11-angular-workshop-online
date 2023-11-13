64975484aec7c107aeb2b652058014894352fe61
Ferdinand Malcher <ferdinand@malcher.media>
2023-11-13 15:50:56 +0100

    Exkurs: HTTPTestingController

--- book-rating/src/app/books/shared/book-store.service.spec.ts
+++ book-rating/src/app/books/shared/book-store.service.spec.ts
@@ -1,7 +1,10 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpTestingController, provideHttpClientTesting }
+    from '@angular/common/http/testing';
 
 import { BookStoreService } from './book-store.service';
 import { of } from 'rxjs';
+import { provideHttpClient } from '@angular/common/http';
 
 describe('BookStoreService', () => {
   let service: BookStoreService;
@@ -10,10 +13,17 @@ describe('BookStoreService', () => {
 
     // so könnte ein Stub für den HttpClient aussehen
     const httpMock = {
-      get: () => of([])
+      get: () => of([]),
+      post: () => of()
     }
 
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        // { provide: HttpClient, useValue: httpMock }
+        provideHttpClient(),
+        provideHttpClientTesting()
+      ]
+    });
     service = TestBed.inject(BookStoreService);
   });
 
